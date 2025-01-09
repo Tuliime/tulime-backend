39e208273b7d7ff93f21486bb4514e97e0751d80
Tibz-Dankan <tibesigwadankan@gmail.com>
2025-01-10 00:49:35 +0300

    refactor: add imagePath to the fields on saving agro product to the db

--- internal/handlers/agroproducts/PostAgroProduct.go
+++ internal/handlers/agroproducts/PostAgroProduct.go
@@ -1,24 +1,16 @@
 package agroproducts
 
 import (
-	"fmt"
-	"log"
-	"math/rand"
-	"os"
-	"strconv"
-
 	"github.com/Tuliime/tulime-backend/internal/models"
 	"github.com/Tuliime/tulime-backend/internal/packages"
 	"github.com/gofiber/fiber/v2"
 )
 
-// TODO: Validate file presences
 var PostAgroProduct = func(c *fiber.Ctx) error {
 	agroProduct := models.Agroproduct{}
 
 	agroProduct.Name = c.FormValue("name")
 	agroProduct.Category = c.FormValue("category")
-	log.Printf("agroProduct: %+v", agroProduct)
 
 	if agroProduct.Name == "" || agroProduct.Category == "" {
 		return fiber.NewError(fiber.StatusBadRequest, "Missing name/category!")
@@ -34,20 +26,14 @@ var PostAgroProduct = func(c *fiber.Ctx) error {
 	if file.Size > maxFileSize {
 		return fiber.NewError(fiber.StatusBadRequest, "File size exceeds the 10 MB limit")
 	}
-
-	log.Println("About to generate random number")
-
-	randNumStr := strconv.Itoa(rand.Intn(9000) + 1000)
-	env := os.Getenv("GO_ENV")
-	filePath := "tulime/" + env + "/" + randNumStr + "_" + file.Filename
-
-	firebaseStorage := packages.FirebaseStorage{FilePath: filePath}
-
 	fileReader, err := file.Open()
 	if err != nil {
 		return fiber.NewError(fiber.StatusBadRequest, err.Error())
 	}
 	defer fileReader.Close()
+
+	filePath := packages.GenFilePath(file.Filename)
+	firebaseStorage := packages.FirebaseStorage{FilePath: filePath}
 
 	imageUrl, err := firebaseStorage.Add(fileReader, file)
 	if err != nil {
@@ -55,7 +41,7 @@ var PostAgroProduct = func(c *fiber.Ctx) error {
 	}
 
 	agroProduct.ImageUrl = imageUrl
-	fmt.Println("imageUrl :", imageUrl)
+	agroProduct.ImagePath = filePath
 
 	newAgroProduct, err := agroProduct.Create(agroProduct)
 	if err != nil {
