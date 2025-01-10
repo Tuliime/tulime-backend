ace73b5e774e3356108e4e9c53a5f5209c716b16
Tibz-Dankan <tibesigwadankan@gmail.com>
2025-01-10 16:18:58 +0300

    feat: add an optional query limit when fetching agroproducts

--- internal/handlers/agroproducts/getAllAgroProducts.go
+++ internal/handlers/agroproducts/getAllAgroProducts.go
@@ -2,6 +2,7 @@ package agroproducts
 
 import (
 	"github.com/Tuliime/tulime-backend/internal/models"
+	"github.com/Tuliime/tulime-backend/internal/packages"
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -24,15 +25,26 @@ import (
 
 var GetAllAgroProducts = func(c *fiber.Ctx) error {
 	agroProduct := models.Agroproduct{}
+	limitParam := c.Query("limit")
 
-	agroProducts, err := agroProduct.FindAll()
+	limit, err := packages.ValidateQueryLimit(limitParam)
 	if err != nil {
 		return fiber.NewError(fiber.StatusBadRequest, err.Error())
 	}
 
+	agroProducts, err := agroProduct.FindAll(limit)
+	if err != nil {
+		return fiber.NewError(fiber.StatusBadRequest, err.Error())
+	}
+
+	pagination := map[string]interface{}{
+		"limit": limit,
+	}
+
 	response := fiber.Map{
-		"status": "success",
-		"data":   agroProducts,
+		"status":     "success",
+		"data":       agroProducts,
+		"pagination": pagination,
 	}
 
 	return c.Status(fiber.StatusOK).JSON(response)
