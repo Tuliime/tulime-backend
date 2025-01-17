e926fc9a086884740610bcd59d93cc1eaa66ca11
Tibz-Dankan <tibesigwadankan@gmail.com>
2025-01-17 15:27:12 +0300

    feat: add cursor-based pagination and category filter to the farm inputs endpoint

--- internal/handlers/farminputs/getAllFarmInputs.go
+++ internal/handlers/farminputs/getAllFarmInputs.go
@@ -9,19 +9,34 @@ import (
 var GetAllFarmInputs = func(c *fiber.Ctx) error {
 	farmInputs := models.FarmInputs{}
 	limitParam := c.Query("limit")
+	categoryParam := c.Query("category")
+	cursorParam := c.Query("cursor")
 
 	limit, err := packages.ValidateQueryLimit(limitParam)
 	if err != nil {
 		return fiber.NewError(fiber.StatusBadRequest, err.Error())
 	}
 
-	allFarmInputs, err := farmInputs.FindAll(limit)
+	if cursorParam == "" {
+		cursorParam = ""
+	}
+	if categoryParam == "" {
+		categoryParam = ""
+	}
+
+	allFarmInputs, err := farmInputs.FindAll(limit, categoryParam, cursorParam)
 	if err != nil {
 		return fiber.NewError(fiber.StatusBadRequest, err.Error())
 	}
 
+	var prevCursor string
+	if len(allFarmInputs) > 0 {
+		prevCursor = allFarmInputs[len(allFarmInputs)-1].ID
+	}
+
 	pagination := map[string]interface{}{
-		"limit": limit,
+		"limit":      limit,
+		"prevCursor": prevCursor,
 	}
 
 	response := fiber.Map{
