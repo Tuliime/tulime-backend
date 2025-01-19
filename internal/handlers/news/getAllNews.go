796995594e480819c629df8ec7796ea67b964fcc
Tibz-Dankan <tibesigwadankan@gmail.com>
2025-01-19 15:50:46 +0300

    feat: add pagination and category filtering to the get all news endpoint

--- internal/handlers/news/getAllNews.go
+++ internal/handlers/news/getAllNews.go
@@ -9,19 +9,34 @@ import (
 var GetAllNews = func(c *fiber.Ctx) error {
 	news := models.News{}
 	limitParam := c.Query("limit")
+	categoryParam := c.Query("category")
+	cursorParam := c.Query("cursor")
 
 	limit, err := packages.ValidateQueryLimit(limitParam)
 	if err != nil {
 		return fiber.NewError(fiber.StatusBadRequest, err.Error())
 	}
 
-	allNews, err := news.FindAll(limit)
+	if cursorParam == "" {
+		cursorParam = ""
+	}
+	if categoryParam == "" {
+		categoryParam = ""
+	}
+
+	allNews, err := news.FindAll(limit, categoryParam, cursorParam)
 	if err != nil {
 		return fiber.NewError(fiber.StatusBadRequest, err.Error())
 	}
 
+	var prevCursor string
+	if len(allNews) > 0 {
+		prevCursor = allNews[len(allNews)-1].ID
+	}
+
 	pagination := map[string]interface{}{
-		"limit": limit,
+		"limit":      limit,
+		"prevCursor": prevCursor,
 	}
 
 	response := fiber.Map{
